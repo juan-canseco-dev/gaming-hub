c04cb1bba1fb642507070a4195fa1ff1f58447af
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-02 18:30:29 -0700

    feat(chat) introduce Chat, ChatMessage, ChatMember entites with domain errors

---
+++ src/GameHub.Domain/Chats/MessageErrors.cs
@@ -0,0 +1,15 @@
+﻿using GameHub.Domain.Abstractions;
+
+namespace GameHub.Domain.Chats;
+
+public static class MessageErrors
+{
+    public static Error MessageContentRequired() =>
+        new("Chat.MessageContentRequired",
+            "Message content must be provided.");
+
+    public static Error MessageTooLong(int maxLength) =>
+        new("Chat.MessageTooLong",
+            $"Message content must not exceed {maxLength} characters.");
+
+}
