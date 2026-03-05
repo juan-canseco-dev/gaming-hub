0567373a51a5c686042dfdf63c3022d71beb7a41
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-04 17:05:43 -0700

    feature(application/identity) Add indentity abstractions

---
+++ src/GameHub.Application/Contracts/Identity/RegisterUserRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameHub.Application.Contracts.Identity;
+
+public class RegisterUserRequest
+{
+    [Required]
+    [EmailAddress]
+    [MaxLength(50)]
+    public string Email { get; set; } = default!;
+
+    [Required]
+    [MaxLength(30)]
+    public string Username { get; set; } = default!;
+
+    [Required]
+    [MaxLength(50)]
+    public string Fullname { get; set; } = default!;
+
+    [Required]
+    [MinLength(6)]
+    [MaxLength(30)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = default!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; set; } = default!;
+}
