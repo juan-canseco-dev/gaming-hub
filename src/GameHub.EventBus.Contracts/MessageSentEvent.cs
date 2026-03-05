5f99dcecfc0f66ec3954e1e35aad547bcab7d871
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-05 16:17:00 -0700

    feat(event-bus/contracts): add integration event contracts for messaging and chat membership

---
+++ src/GameHub.EventBus.Contracts/MessageSentEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace GameHub.EventBus.Contracts;
+
+public class MessageSentEvent
+{
+    public Guid UserId { get; init; }
+    public Guid ChatId { get; init; }
+    public Guid MessageId { get; init; }
+}
