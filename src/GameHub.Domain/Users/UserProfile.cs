207608e6a6d9938219ffae2cdb381db297c93dd6
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-04 17:34:55 -0700

    feature(domain/users): add UserProfile entity to domain

---
+++ src/GameHub.Domain/Users/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using GameHub.Domain.Abstractions;
+
+namespace GameHub.Domain.Users;
+
+public class UserProfile : Entity<Guid>
+{
+
+    public string Email { get; private init; } = default!;
+    public string Username { get; private init; } = default!;
+    public string Fullname { get; private init; } = default!;
+    public DateTimeOffset CreatedAt { get; private init; } = default!;
+    private UserProfile() { }
+    public UserProfile(
+        Guid id, 
+        string email,
+        string username, 
+        string fullname,
+        DateTimeOffset createdAt
+    ) : base(id)
+    {
+        Email = email;
+        Username = username;
+        Fullname = fullname;
+        CreatedAt = createdAt;
+    }
+}
