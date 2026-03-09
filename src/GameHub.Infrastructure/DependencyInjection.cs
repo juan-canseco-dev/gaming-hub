19921c592002cc6909387012501d768d50472969
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-09 14:48:50 -0600

    feature(infrastructure): Register ApplicationContext in Dependency Injection container

--- src/GameHub.Infrastructure/DependencyInjection.cs
+++ src/GameHub.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,13 @@
-﻿using GameHub.Application.Abstractions.Clock;
+﻿using GameHub.Application.Abstractions.Authentication;
+using GameHub.Application.Abstractions.Clock;
+using GameHub.Application.Abstractions.Data;
+using GameHub.Infrastructure.Authentication;
 using GameHub.Infrastructure.Clock;
+using GameHub.Infrastructure.Data;
+using GameHub.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +18,34 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddSingleton<IAuthenticatedUserService, AuthenticatedUserService>();
+
+        // Register Ef core dependencies 
+        var connectionString = configuration.GetConnectionString("ConnectionString")
+                          ?? throw new ArgumentNullException(nameof(configuration));
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.UseSqlServer(connectionString);
+            options.EnableSensitiveDataLogging();
+        });
+
+        services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+/ ";
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 6;
+        })
+       .AddRoles<ApplicationRole>()
+       .AddEntityFrameworkStores<ApplicationDbContext>()
+       .AddDefaultTokenProviders();
+
         return services;
     }
 }
