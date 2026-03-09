3f07fd2dba6222f84672992f7855af5049fee690
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-08 22:45:18 -0600

    feature: add authenticated user service impl

---
+++ src/GameHub.Infrastructure/Authentication/AuthenticatedUserService.cs
@@ -0,0 +1,23 @@
+﻿using GameHub.Application.Abstractions.Authentication;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+
+namespace GameHub.Infrastructure.Authentication;
+
+internal sealed class AuthenticatedUserService : IAuthenticatedUserService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+    public AuthenticatedUserService(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor ?? throw new ArgumentNullException(nameof(contextAccessor));
+    }
+    public Guid UserId
+    {
+        get
+        {
+            var uid = _contextAccessor?.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return string.IsNullOrEmpty(uid) ? Guid.Empty : Guid.Parse(uid);
+        }
+    }
+}
