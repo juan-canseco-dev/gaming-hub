ac6bcbbc0dce4b5d0e750d923dd380ca087f12f8
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-08 19:22:11 -0600

    feat(application): add get chat messages feature and tests

---
+++ src/GameHub.Application/Features/Chats/GetMessage/Projections.cs
@@ -0,0 +1,36 @@
+﻿using GameHub.Application.Contracts.Chats;
+using GameHub.Application.Contracts.Profile;
+using GameHub.Domain.Chats;
+using GameHub.Domain.Users;
+
+namespace GameHub.Application.Features.Chats.GetMessage;
+
+internal static class GetMessageByIdProjections
+{
+    public static IQueryable<MessageDto> ProjectToDto(
+      this IQueryable<ChatMessage> messages,
+      IQueryable<UserProfile> userProfiles)
+    {
+        return
+            from message in messages
+            join user in userProfiles
+                on message.SenderUserId equals user.Id into users
+            from user in users.DefaultIfEmpty()
+            select new MessageDto
+            {
+                Id = message.Id,
+                Content = message.Content,
+                CreatedAt = message.CreatedAt.UtcDateTime,
+                IsSystem = message.Type == ChatMessageType.System,
+                User = user == null
+                    ? null
+                    : new UserDto
+                    {
+                        Id = user.Id,
+                        Username = user.Username,
+                        Email = user.Email,
+                        Fullname = user.Fullname
+                    }
+            };
+    }
+}
