62f9d98c09252d20ce34a417a25b3e5428461ac1
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-09 10:33:50 -0600

    feature(infrastructure) add dependency injection container

---
+++ src/GameHub.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using GameHub.Application.Abstractions.Clock;
+using GameHub.Infrastructure.Clock;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameHub.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        return services;
+    }
+}
