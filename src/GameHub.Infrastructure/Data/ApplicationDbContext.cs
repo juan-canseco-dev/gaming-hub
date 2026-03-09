24efd96bea8508f22654c3906ed9901f9ead67a5
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2026-03-08 23:02:35 -0600

    feature: add application db context basic impl with ApplicationUser and ApplicationRole identity models

---
+++ src/GameHub.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GameHub.Application.Abstractions.Data;
+using GameHub.Domain.Chats;
+using GameHub.Domain.Users;
+using GameHub.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameHub.Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) {}
+
+    public DbSet<Channel> Channels => Set<Channel>();
+
+    public DbSet<Chat> Chats => Set<Chat>();
+
+    public DbSet<ChatMessage> ChatMessages => Set<ChatMessage>();
+
+    public DbSet<ChatMember> ChatMembers => Set<ChatMember>();
+
+    public DbSet<UserProfile> UserProfiles => Set<UserProfile>();
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        => base.SaveChangesAsync(cancellationToken);
+}
